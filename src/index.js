c133874530a3ca8588b9037271bef7152adb7bb0
bukerok <bukerok@users.noreply.github.com>
2021-05-17 23:58:11 +0300

    [DEV] reorganaize project structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+
+import App from './App';
+import { store } from './store/store';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/pages/statistics.js
@@ -0,0 +1,9 @@
+const StatisticsPage = () => {
+  return (
+    <div>
+      Statistics Page component
+    </div>
+  );
+};
+
+export default StatisticsPage;
