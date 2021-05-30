e17de2c3a4e1d034073c1f16cd4b52121c9ab7b1
bukerok <bukerok@users.noreply.github.com>
2021-05-30 11:25:08 +0300

    [BUG] fix shooting panel tests data

--- src/components/ShootingPanel/index.test.js
+++ src/components/ShootingPanel/index.test.js
@@ -5,10 +5,28 @@ import ShootingPanel from '.';
 jest.mock('react-router-dom', () => ({
   Link: 'a',
 }));
+jest.mock('react-redux', () => ({
+  useSelector: () => [
+    {
+      date: 'mockDate',
+      zone: 12,
+      score: 3,
+      attempts: 15,
+    },
+    {
+      date: 'mockDate2',
+      zone: 1,
+      score: 5,
+      attempts: 10,
+    },
+  ],
+}));
+jest.mock('../../store/features/shots/shotsSlice', () => ({
+  selectRecords: () => {},
+}));
 
 describe('ShootingPanel', () => {
   it('renders correctly', () => {
-
     const tree = renderer.create(<ShootingPanel />)
       .toJSON();
 
