d96e33d4b5ca19033da488e151013487bbbfb8ef
bukerok <bukerok@users.noreply.github.com>
2021-06-27 17:13:20 +0300

    [DEV] add tests for statistics helpers

--- src/helpers/statistics.test.js
+++ src/helpers/statistics.test.js
@@ -1,4 +1,8 @@
-import { calculateStats } from './statistics';
+import {
+  calculateStats,
+  calculateStat,
+  calculateZoneStats,
+} from './statistics';
 
 describe('statistics helper', () => {
   describe('calculateStats', () => {
@@ -28,6 +32,58 @@ describe('statistics helper', () => {
       expect(calculateStats([]).ft).toStrictEqual({
         score: 0,
         attempts: 0,
+      });
+    });
+  });
+
+  describe('calculateStat', () => {
+    it('should return null if no data', () => {
+      expect(calculateStat({})).toBe(null);
+    });
+
+    it('should return null if zeros provided', () => {
+      expect(calculateStat({
+        score: 0,
+        attempts: 0,
+      })).toBe(null);
+    });
+  });
+
+  describe('calculateZoneStats', () => {
+    it('should return empty object if no data', () => {
+      expect(calculateZoneStats()).toStrictEqual({});
+    });
+
+    it('should accumulate records by zone', () => {
+      const mockRecords = [
+        {
+          date: 'mockDate',
+          zone: 2,
+          score: 7,
+          attempts: 13,
+        },
+        {
+          date: 'mockDate2',
+          zone: 4,
+          score: 3,
+          attempts: 7,
+        },
+        {
+          date: 'mockDate3',
+          zone: 2,
+          score: 13,
+          attempts: 27,
+        }
+      ];
+      const result = calculateZoneStats(mockRecords);
+
+      expect(result[2]).toStrictEqual({
+        score: 20,
+        attempts: 40,
+      });
+      expect(result[4]).toStrictEqual({
+        score: 3,
+        attempts: 7,
       });
     });
   });
